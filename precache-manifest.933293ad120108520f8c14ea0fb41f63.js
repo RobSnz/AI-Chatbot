bb7fd78e26c06e6632a1a10c6f212baca756f6a7
Breahem-Bedi <lovelybedi@hotmail.com>
2020-05-20 23:35:51 +1200

    Updates

---
+++ precache-manifest.933293ad120108520f8c14ea0fb41f63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9114f5aa8b37bc7094cf2a9e96a0bc51",
+    "url": "/AI-Chatbot/index.html"
+  },
+  {
+    "revision": "a80b7529c707887bdbbd",
+    "url": "/AI-Chatbot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2c640bb1713bd2ab201",
+    "url": "/AI-Chatbot/static/js/2.9dbbff43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AI-Chatbot/static/js/2.9dbbff43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80b7529c707887bdbbd",
+    "url": "/AI-Chatbot/static/js/main.452e4e16.chunk.js"
+  },
+  {
+    "revision": "38db2010a390fb36bfc7",
+    "url": "/AI-Chatbot/static/js/runtime-main.7a8a5b1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AI-Chatbot/static/media/logo.5d5d9eef.svg"
+  }
+]);
