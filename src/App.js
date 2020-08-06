f39227c67741f24db0a1ed46ae3dd86ca8c37d9d
RobSnz <robert.smitnz@gmail.com>
2020-08-07 10:43:29 +1200

    updated anime.js loading

--- src/App.js
+++ src/App.js
@@ -140,12 +140,12 @@ class App extends React.Component {
               ]}
             >
             <ul>
-              <li id="Box" style={{ height: 25, width: 25, background: "#194a70", listStyleType: "none", marginTop: "0px" }} />
-              <li id="Box" style={{ height: 25, width: 25, background: "#194a70", listStyleType: "none", marginTop: "-25px", marginLeft: "200px" }} />
-              <li id="Box" style={{ height: 25, width: 25, background: "#194a70", listStyleType: "none", marginTop: "-25px", marginLeft: "170px" }} />
-              <li id="Box" style={{ height: 25, width: 25, background: "#194a70", listStyleType: "none", marginTop: "-25px", marginLeft: "140px"  }} />
-              <li id="Box" style={{ height: 25, width: 25, background: "#194a70", listStyleType: "none", marginTop: "-25px", marginLeft: "110px" }} />
-              <li id="Box" style={{ height: 25, width: 25, background: "#194a70", listStyleType: "none", marginTop: "-25px", marginLeft: "80px" }} />
+              <li id="Box" style={{ height: 25, width: 25, background: "#61658B", listStyleType: "none", borderRadius: "20px", marginTop: "0px" }} />
+              <li id="Box" style={{ height: 25, width: 25, background: "#61658B", listStyleType: "none", borderRadius: "20px", marginTop: "-25px", marginLeft: "200px" }} />
+              <li id="Box" style={{ height: 25, width: 25, background: "#61658B", listStyleType: "none", borderRadius: "20px", marginTop: "-25px", marginLeft: "170px" }} />
+              <li id="Box" style={{ height: 25, width: 25, background: "#61658B", listStyleType: "none", borderRadius: "20px", marginTop: "-25px", marginLeft: "140px"  }} />
+              <li id="Box" style={{ height: 25, width: 25, background: "#61658B", listStyleType: "none", borderRadius: "20px", marginTop: "-25px", marginLeft: "110px" }} />
+              <li id="Box" style={{ height: 25, width: 25, background: "#61658B", listStyleType: "none", borderRadius: "20px", marginTop: "-25px", marginLeft: "80px" }} />
             </ul>
             </Anime>
           </div>
