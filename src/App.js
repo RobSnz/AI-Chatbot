f9f948394b466e7e0a1fa5d484f30b443aacd576
RobSnz <robert.smitnz@gmail.com>
2020-08-06 12:25:54 +1200

    Updated robot animation, added anime.js, updated textbox, updated "key" warning

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import chatHead from './images/Avatar-Icon.png';
 import userHead from './images/userT.png';
 import styles from './mystyle.module.css';
 import Robot from "./components/Robot";
-import Man from "./components/Man";
+import Man2 from "./components/Man2";
+import ReactAnime from 'react-animejs';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { query: '' };
+    this.state = { query: '', avatar: 'robot', counter: 0 };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -27,6 +28,7 @@ class App extends React.Component {
   // reset query to be blank
   handleSubmit(event) {
     event.preventDefault();
+    
     const { query } = this.state;
     const { sendMessage } = this.props;
 
@@ -61,6 +63,10 @@ class App extends React.Component {
     }
 
     var minutes = entry.date.getMinutes();
+    
+    if(minutes < 10) {
+      minutes = "0" + minutes;
+    }
 
     var hours;
 
@@ -75,30 +81,60 @@ class App extends React.Component {
 
   render() {
     const { messages } = this.props;
-
+    const { Anime, Stagger } = ReactAnime;
+    
     return (
       <div>
         <div>
           <div className={ styles.chatWindowStyle }>
             <ul className={ styles.queryBoxStyle } id="messageBox">
                 { messages.map(entry => {
+                  this.state.counter++;
                   if(entry.date == null) {
                     this.setDate(entry);
                   }
 
                   if(entry.sender === "user") {
-                    return <div><img src={ userHead } alt='user' className={ styles.imgStyle }></img><li className={ styles.userStyle }><li className={ styles.titleStyle }>User { entry.date }</li><br />{ entry.text }</li></div>;
+                    return <div key={this.state.counter}><img src={ userHead } alt='user' className={ styles.imgStyle }></img><ul className={ styles.userStyle }><li className={ styles.titleStyle }>User { entry.date }</li><br />{ entry.text }</ul></div>;
                   } else {
-                    return <div><img src={ chatHead } alt='chatbot' className={ styles.imgStyle }></img><li className={ styles.chatbotStyle }><li className={ styles.titleStyle }>Chatbot { entry.date }</li><br />{ entry.text }</li></div>;
+                    return <div key={this.state.counter}><img src={ chatHead } alt='chatbot' className={ styles.imgStyle }></img><ul className={ styles.chatbotStyle }><li className={ styles.titleStyle }>Chatbot { entry.date }</li><br />{ entry.text }</ul></div>;
                   }
                 })}
             </ul>
             
             <form onSubmit={ this.handleSubmit } className={ styles.inputBoxStyle }>
-              <input type='text' placeholder='Enter Query!' onChange={ this.handleChange } value={ this.state.query } className={ styles.fontChoice }/>
+              <textarea onKeyDown={ (e) => { if(e.keyCode === 13) this.handleSubmit(e);}} style={{ width: "200px", height: "50px", overflowWrap: "break-word"}} type='text' placeholder='Enter Query!' onChange={ this.handleChange } value={ this.state.query } className={ styles.fontChoice }/>
             </form>
           </div>
         </div>
+
+        <Anime
+          initial={[
+            {
+              targets: "#Box",
+              translateX: 50,
+              easing: "linear",
+              loop: true,
+              duration: 10000,
+              keyframes: [
+                {
+                  translateX: 630
+                },
+                {
+                  translateY: 700
+                },
+                {
+                  translateX: 0
+                },
+                {
+                  translateY: 0
+                }
+              ],
+            }
+          ]}
+        >
+          <div id="Box" style={{ height: 50, width: 50, background: "#194a70", marginLeft: "60%" }} />
+        </Anime>
         <Robot></Robot>
       </div>
     )
